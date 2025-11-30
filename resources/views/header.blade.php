8cf3f99d7f7fb0720f0afa0342eefdc5587087d1
Federico BC <federovic1325@gmail.com>
2025-11-30 03:00:08 +0100

    header completado

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,27 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @yield("head")
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
-</head>
-<body>
-    <header class="navbar navbar-light bg-light px-3 shadow-sm">
-  <a class="navbar-brand mb-0 h1" href="#">
-    BookHub
-  </a>
 
-  <div class="d-flex align-items-center">
-    <button class="btn btn-outline-secondary btn-sm me-2">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <button class="btn btn-outline-secondary btn-sm">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-  </div>
-</header>
-@yield("body")
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @yield("head")
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+</head>
+
+<body>
+  <header class="navbar navbar-light bg-light px-3 shadow-sm">
+    <div class="d-flex align-items-left">
+      <h1 class="navbar-brand card-title text-center" style="color: orange; font-weight: bold">BookHub</h1>
+      <a class="navbar-brand card-title text-center" href="">Inicio</a>
+      <a class="navbar-brand card-title text-center" href="">Perfil</a>
+    </div>
+
+    <div class="d-flex align-items-center">
+      <button class="nav-item dropdown">
+        <a href="#" role="button" data-bs-toggle="dropdown" class="nav-link dropdown-toggle">
+          Nombre de usuario
+          <img src="ProfilePictures/defaultPFP.png" alt="Foto de perfil" class="rounded-circle ms-2" width="30" height="30">
+        </a>
+        <ul class="dropdown-menu">
+          <li><a href="#" class="dropdown-item">Ajustes de cuenta</a></li>
+          <li><a href="#" class="dropdown-item text-danger">Cerrar sesión</a></li>
+        </ul>
+      </button>
+    </div>
+  </header>
+  @yield("body")
 </body>
+
 </html>
