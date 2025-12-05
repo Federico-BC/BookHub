5dbfaa6806ece87e94a0b31ea3f35f9edef3c7f3
Federico BC <108863764+Federico-BC@users.noreply.github.com>
2025-12-05 23:32:11 +0100

    Feat search de usuario terminado (#20)

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ Route::get("/search", [PageController::class, "search"])->name("search");
 Route::post("/book/addFav", [ServerController::class, "addFavoriteBook"])->name("addFav");
 Route::post("book/removeFav", [ServerController::class, "removeFavoriteBook"])->name("removeFav");
 Route::get("/book/{olid}", [PageController::class, "book"])->name("book");
-Route::get("/profile/{username}", [PageController::class, "profile"])->name("profile");
+Route::get('/profile/{username}', [PageController::class, 'profile'])->name('profile');
 Route::get("/editProfile", [PageController::class, "editProfile"])->name("accountSettings");
 Route::post("/editProfile/save", [ServerController::class, "saveProfileChanges"])->name("saveProfileChanges");
+Route::get('/search-user', [PageController::class, 'searchUser'])->name('search.user');
